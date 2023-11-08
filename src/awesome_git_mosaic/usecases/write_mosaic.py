be80992a4a259856077b6207a8aa9df4fe8b0a10
Giovane Costa <gigio.coder@gmail.com>
2023-11-08 12:29:46 -0300

    Change modify and commit strategy

--- src/awesome_git_mosaic/usecases/write_mosaic.py
+++ src/awesome_git_mosaic/usecases/write_mosaic.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from random import random
 
 from awesome_git_mosaic.adapters.git_mosaic.git_mosaic_adapter import GitMosaicAdapter
 from awesome_git_mosaic.gateways.git.git_gateway import GitGateway
@@ -26,7 +25,7 @@ class WriteMosaic:
         background: bool = False,
         inverted: bool = False,
     ):
-        timestamps = []
+        timestamps: list[datetime] = []
         if background:
             bgstr = " " * (len(message) * multiply)
             timestamps += self.git_mosaic_adapter.output(
@@ -43,12 +42,15 @@ class WriteMosaic:
         total = len(timestamps)
         count = 0.0
         last_pct = 0
-        print(f"Creating {total} commits...")
+
+        print(f"Disabling git Garbage Collector...")
         self.git_gateway.disable_garbage_collector()
+
+        print(f"Creating {total} commits...")
         for timestamp in timestamps:
-            self.modify_file.modify()
+            self.modify_file.modify(timestamp.ctime())
             self.git_gateway.add()
-            self.git_gateway.commit(f"{timestamp.ctime()} {random()}", timestamp)
+            self.git_gateway.commit(f"{timestamp.ctime()} {count}", timestamp)
 
             count += 1
             pct = round(count * 100 / total)
@@ -57,5 +59,14 @@ class WriteMosaic:
                 print(f"{pct}%")
                 last_pct = pct
 
+            if count % 500 == 0:
+                print(f"{count} commits created, pushing")
+                self.git_gateway.push()
+
+        print("All commits created. Last push...")
         self.git_gateway.push()
+
+        print(f"Re-enabling git Garbage Collector...")
         self.git_gateway.enable_garbage_collector()
+
+        print("That's all, folks! Bye!")
